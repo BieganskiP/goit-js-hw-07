540c694310efb221764e7132e44f9f68c322e42e
BieganskiP <bieganski1996@gmail.com>
2022-11-22 02:33:03 +0100

    a

---
+++ js/02-lightbox.js
@@ -0,0 +1,18 @@
+import { galleryItems } from "./gallery-items.js";
+// Change code below this line
+
+console.log(galleryItems);
+
+const list = document.querySelector(".gallery");
+
+galleryItems.forEach((image) => {
+  list.insertAdjacentHTML(
+    "beforeend",
+    `<li class="gallery__item"><a href="${image.original}"><img src="${image.preview}" alt="${image.description}" class="gallery__image"></img></a></li>`
+  );
+});
+
+var lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
